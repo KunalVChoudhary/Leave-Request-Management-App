d725cfd5d92fdc6ef3b901dfaa028add64b20ddb
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2026-03-09 09:18:42 +0530

    Created mongoose Schema for Users collection

---
+++ backend/models/user.js
@@ -0,0 +1,29 @@
+const {Schema,model} = require('mongoose')
+
+const userSchema= new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role: {
+        type: String,
+        enum: ["employee", "employer"],
+        default: "employee",
+        required: true
+    }
+},{
+    timestamps:true
+})
+
+const User=model('user',userSchema)
+
+module.exports=User
